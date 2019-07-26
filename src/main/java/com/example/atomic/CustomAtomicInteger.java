57a506489ace046957216dbdeda84be2f4143fbe
xieyufeng <yufengxiehn@163.com>
2019-07-26 10:47:21 +0800

    atomic

--- src/main/java/com/example/atomic/CustomAtomicInteger.java
+++ src/main/java/com/example/atomic/CustomAtomicInteger.java
@@ -1,5 +1,7 @@
 package com.example.atomic;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @ClassName: CustomAtomicInteger
  * @Description:
@@ -9,7 +11,37 @@ package com.example.atomic;
  * @Date 2019/7/16 08:47
  */
 public class CustomAtomicInteger {
+    private static AtomicInteger atomicI = new AtomicInteger(0);
+    public static void increment() {
+        for (;;) {
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+    public static final int THREAD_COUNT = 1;
     public static void main(String[] args) {
+        CustomAtomicInteger cai = new CustomAtomicInteger();
 
+        Thread[] threads = new Thread[THREAD_COUNT];
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        increment();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() == 1) {
+            Thread.yield();
+        }
+        System.out.println("atomicI=" + atomicI);
     }
 }
