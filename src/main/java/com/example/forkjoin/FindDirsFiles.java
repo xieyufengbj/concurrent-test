f8ba7d411f7314f1966994f7a63577ac72305703
xieyufeng <yufengxiehn@163.com>
2019-08-05 18:55:57 +0800

    fork-join

--- src/main/java/com/example/forkjoin/FindDirsFiles.java
+++ src/main/java/com/example/forkjoin/FindDirsFiles.java
@@ -3,6 +3,7 @@ package com.example.forkjoin;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 /**
@@ -13,33 +14,54 @@ import java.util.concurrent.RecursiveAction;
  */
 public class FindDirsFiles extends RecursiveAction {
 
-    // 当前任务需要搜寻的目录
-    private File path;
+    private File path;//当前任务需要搜寻的目录
 
     public FindDirsFiles(File path) {
         this.path = path;
     }
 
+    public static void main(String [] args){
+        try {
+            // 用一个 ForkJoinPool 实例调度总任务
+            ForkJoinPool pool = new ForkJoinPool();
+            FindDirsFiles task = new FindDirsFiles(new File("F:/"));
+
+            pool.execute(task);//异步调用
+
+            System.out.println("Task is Running......");
+            Thread.sleep(1);
+            int otherWork = 0;
+            for(int i=0;i<100;i++){
+                otherWork = otherWork+i;
+            }
+            System.out.println("Main Thread done sth......,otherWork="+otherWork);
+            task.join();//阻塞的方法
+            System.out.println("Task end");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void compute() {
-//        List<FindDirsFiles> subTasks = new ArrayList<>();
-//        File[] files = path.listFiles();
-//        if (files != null) {
-//            for (File file : files) {
-//                if (file.isDirectory()) {
-//                    subTasks.add(new FindDirsFiles(file));
-//                } else {
-//                    if (file.getAbsolutePath().endsWith("txt")) {
-//                        System.out.println("文件：" + file.getAbsolutePath());
-//                    }
-//                }
-//            }
-//            if (!subTasks.isEmpty()) {
-//                for (FindDirsFiles subTask : invokeAll(subTasks)) {
-//                    // 等待子任务执行完成
-//                    subTask.join();
-//                }
-//            }
-//        }
+        List<FindDirsFiles> subTasks = new ArrayList<>();
+        File[] files = path.listFiles();
+        if(files!=null) {
+            for(File file:files) {
+                if(file.isDirectory()) {
+                    subTasks.add(new FindDirsFiles(file));
+                }else {
+                    //遇到文件，检查
+                    if(file.getAbsolutePath().endsWith("txt")) {
+                        System.out.println("文件："+file.getAbsolutePath());
+                    }
+                }
+            }
+            if(!subTasks.isEmpty()) {
+                for(FindDirsFiles subTask:invokeAll(subTasks)) {
+                    subTask.join();//等待子任务执行完成
+                }
+            }
+        }
     }
 }
